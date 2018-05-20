a673f76cea2d50cef9a71644b43a4b504a4d9631
pavlicic1marko <39461512+pavlicic1marko@users.noreply.github.com>
2018-05-20 20:45:26 +0200

    Create bull-cow.py

---
+++ bull-cow.py
@@ -0,0 +1,18 @@
+from random import randint
+def C_B():
+    random='1234'
+    user=input('enter a four digit number:')
+    cow=0
+    bull=0
+    for i in range(4):
+        if random[i] == user[i]:
+            cow+=1
+    for i in range(4):
+        for x in range(4):
+            if user[i] in random[x]:
+                random=random.strip(user[i])
+                random=random.__add__('c')
+                bull+=1
+    bull=bull-cow
+
+C_B()
