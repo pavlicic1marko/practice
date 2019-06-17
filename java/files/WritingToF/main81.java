b9d10562181f88b62dbc9937cdf8379183bd99f4
pavlicic1marko <39461512+pavlicic1marko@users.noreply.github.com>
2019-06-17 22:27:10 +0200

    Create main81.java

---
+++ java/files/WritingToF/main81.java
@@ -0,0 +1,11 @@
+public class main81 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		files81 object1= new files81();
+		object1.openFile();
+		object1.readFile();
+		object1.closeFile();
+	}
+
+}
