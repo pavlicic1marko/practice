198e5bdf823d3b212e3bcfc9d254ac7587127f91
pavlicic1marko <39461512+pavlicic1marko@users.noreply.github.com>
2019-07-28 08:34:24 +0200

    Create SerializeApplication.java

---
+++ java/serialization/SerializeApplication.java
@@ -0,0 +1,68 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+class SerializeApplication 
+{ 
+    public static void main(String[] args) 
+    {    
+        SerializeThis object = new SerializeThis(1, "john doe"); 
+        String filename = "file.ser"; 
+          
+        // Serialization  
+        try
+        {    
+            //Saving of object in a file 
+            FileOutputStream file = new FileOutputStream(filename); 
+            ObjectOutputStream out = new ObjectOutputStream(file); 
+              
+            // Method for serialization of object 
+            out.writeObject(object); 
+              
+            out.close(); 
+            file.close(); 
+              
+            System.out.println("Object has been serialized"); 
+  
+        } 
+          
+        catch(IOException ex) 
+        { 
+            System.out.println("IOException is caught"); 
+        } 
+  
+  
+        SerializeThis object1 = null; 
+  
+        // Deserialization 
+        try
+        {    
+            // Reading the object from a file 
+            FileInputStream file = new FileInputStream(filename); 
+            ObjectInputStream in = new ObjectInputStream(file); 
+              
+            // Method for deserialization of object 
+            object1 = (SerializeThis)in.readObject(); 
+              
+            in.close(); 
+            file.close(); 
+              
+            System.out.println("Object has been deserialized "); 
+            System.out.println("a = " + object1.a); 
+            System.out.println("b = " + object1.b); 
+        } 
+          
+        catch(IOException ex) 
+        { 
+            System.out.println("IOException is caught"); 
+        } 
+          
+        catch(ClassNotFoundException ex) 
+        { 
+            System.out.println("ClassNotFoundException is caught"); 
+        } 
+  
+    } 
+} 
